e6e274bdbe6eae2bf8c50a9cbf23257f48face08
krrish009 <krish1135.becse24@chitkara.edu.in>
2025-02-08 13:02:08 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    if(n>=90){
+        printf("A");
+    }
+    else if(n<90 && n>=80){
+        printf("B");
+    }
+     else if(n<80 && n>=70){
+        printf("C");
+    }
+     else if(n<70 && n>=60){
+        printf("D");
+    }
+    else{
+        printf("F")
+    }
+    return 0;
+}
