4161f62624beb3efe1de84ac4064426b153c4155
Krish Rajput <krrishrajput307@gmail.com>
2025-03-17 21:25:59 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void fibonacci(int n) {
+    int a = 0, b = 1, next;
+    
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a);
+        next = a + b;
+        a = b;
+        b = next;
+    }
+}
+
+
