31b11822ab7a3e20e14e3c0af4c8ed67bac12646
Krish Rajput <krrishrajput307@gmail.com>
2025-03-30 21:30:53 +0530

    Solution to Find the Frequency of Elements in an Array

--- Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
+++ Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
@@ -1,67 +1,27 @@
-// #include <stdio.h>
-
-// int main(){
-//     int n;
-//     scanf("%d",&n);
-//     int arr[n];
-//     for(int i=0;i<n;i++){
-//         scanf("%d",&arr[i]);
-//     }
-//     int visited[n];
-//     for(int i=0;i<n;i++){
-//         visited[i]=0;
-//     }
-//     int count=1;
-//     for(int i=0;i<n;i++){
-//         if(visited[i]==1){
-//             continue;
-//         }
-//         for(int j=i+1;j<n;j++){
-//             if(arr[i]==arr[j]){
-//                 count++;
-//             }
-//         }
-//         printf("%d %d\n",arr[i],count);
-//     }
-// }
 #include <stdio.h>
 
-int main() {
-    int n, i, j;
-
-    // Read the size of the array
-    scanf("%d", &n);
-
-    // Allocate memory for the array
+int main(){
+    int n;
+    scanf("%d",&n);
     int arr[n];
-
-    // Read the array elements
-    for (i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
     }
-
-    // Array to keep track of visited elements
     int visited[n];
-    for (i = 0; i < n; i++) {
-        visited[i] = 0; // Initialize all elements as not visited
+    for(int i=0;i<n;i++){
+        visited[i]=0;
     }
-
-    // Find and print the frequency of unique elements
-    for (i = 0; i < n; i++) {
-        if (visited[i] == 1) {
-            continue; // Skip if already visited
+    int count=1;
+    for(int i=0;i<n;i++){
+        if(visited[i]==1){
+            continue;
         }
-
-        int count = 1; // Count the current element
-        for (j = i + 1; j < n; j++) {
-            if (arr[i] == arr[j]) {
+        for(int j=i+1;j<n;j++){
+            if(arr[i]==arr[j]){
                 count++;
-                visited[j] = 1; // Mark the duplicate as visited
+                visited[j]=1;
             }
         }
-
-        printf("%d %d\n", arr[i], count);
+        printf("%d %d\n",arr[i],count);
     }
-
-    return 0;
 }
