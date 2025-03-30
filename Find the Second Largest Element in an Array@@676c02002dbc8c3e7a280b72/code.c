f7e33748e91f2a046c1d7a15d2b9f9e9534687c3
Krish Rajput <krrishrajput307@gmail.com>
2025-03-30 13:21:29 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -24,28 +24,46 @@ int enterArray(int arr[],int n){
         scanf("%d",&arr[i]);
     }
 }
+
+int findSecondLargest(int arr[], int n) {
+    if (n < 2) return -1;  // If less than 2 elements, return -1
+
+    bubblesort(arr, n);  // Sort in descending order
+
+    int largest = arr[0];  // First element is the largest after sorting
+
+    // Find the first element smaller than `largest`
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < largest) {
+            return arr[i];  // Found second largest
+        }
+    }
+
+    return -1;  // If no second largest found
+}
 int main(){
     int n;
     scanf("%d",&n);
     int arr[n];
     enterArray(arr,n);
-    bubblesort(arr,n);
-    int largest = arr[0];  // First element is the largest after sorting
+    // bubblesort(arr,n);
+    // int largest = arr[0];  // First element is the largest after sorting
 
-    // Find the first element smaller than `largest`
-    for (int i = 1; i < n; i++) {
-        if(n<2){
-            printf("-1");
-        }
-        if (arr[i] < largest) {
-            printf("%d",arr[i]);
-            break;  // Found second largest
-        }
-        else{
-            printf("-1");
-            break;
-        }
-    }
+    // // Find the first element smaller than `largest`
+    // for (int i = 1; i < n; i++) {
+    //     if(n<2){
+    //         printf("-1");
+    //     }
+    //     if (arr[i] < largest) {
+    //         printf("%d",arr[i]);
+    //         break;  // Found second largest
+    //     }
+    //     else{
+    //         printf("-1");
+    //         break;
+    //     }
+    // }
+    printf("%d\n", findSecondLargest(arr, n));  // Print result
 
     
     
