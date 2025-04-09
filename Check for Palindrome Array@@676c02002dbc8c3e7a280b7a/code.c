26dfddaf77c33e32befc7f358238e5d4138853dd
Krish Rajput <krish1135.becse24@chitkara.edu.in>
2025-04-09 10:47:36 +0530

    Solution to Check for Palindrome Array

--- Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
+++ Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
@@ -13,7 +13,7 @@ int main() {
     int n; 
     scanf("%d", &n);
     int arr[n];
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n/2; i++) {
         scanf("%d", &arr[i]);
     }
     if (isPalindromic(arr, n)) {
