4fe0e08b23d680462650d58ae92bca6b42007cec
Krish Rajput <krish1135.becse24@chitkara.edu.in>
2025-04-09 10:20:59 +0530

    Solution to Find the Largest Even Number in an Array

--- Find the Largest Even Number in an Array@@676c02002dbc8c3e7a280b8b/code.c
+++ Find the Largest Even Number in an Array@@676c02002dbc8c3e7a280b8b/code.c
@@ -1,3 +1,25 @@
+// #include <stdio.h>
+
+// int main() {
+//     int n;
+//     scanf("%d", &n);  // Input size of array
+
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//    int maxeven=-1;
+//     for(int i=0;i<n;i++){
+//         if(arr[i]%2==0){
+//             if(arr[i]>maxeven){
+//                 maxeven=arr[i];
+//             }
+//         }
+//     }
+//     printf("%d",maxeven);
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main() {
@@ -5,17 +27,21 @@ int main() {
     scanf("%d", &n);  // Input size of array
 
     int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);  // Input array elements
     }
-   int maxeven=-1;
-    for(int i=0;i<n;i++){
-        if(arr[i]%2==0){
-            if(arr[i]>maxeven){
-                maxeven=arr[i];
+
+    int maxEven = -1;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] % 2 == 0) { // Check even
+            if(arr[i] > maxEven) {
+                maxEven = arr[i];  // Update max even
             }
         }
     }
-    printf("%d",maxeven);
+
+    printf("%d\n", maxEven);  // Print result
     return 0;
 }
+
+
