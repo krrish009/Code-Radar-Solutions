673d6146cbf06ac6e6a30396f65212249f4ab8a3
Krish Rajput <krish1135.becse24@chitkara.edu.in>
2026-08-31 20:12:55 +0530

    Update code.c

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -3,7 +3,7 @@
 void bubbleSort(int arr[], int n) { 
     int temp;
     for (int i = 0; i < n - 1; i++) { 
-        for (int j = 0; j < n - i - 1; j++) { 
+        for (int j = 0; j < n - i - 1; j++)  hjjkhb{ 
             if (arr[j] > arr[j + 1]) { 
                 temp = arr[j];
                 arr[j] = arr[j + 1];
